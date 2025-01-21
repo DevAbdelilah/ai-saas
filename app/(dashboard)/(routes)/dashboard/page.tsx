91eeea48d4c4aff2efe5657dcc30e94daf021f59
DevAbdelilah <charafiabdelilah3@gmail.com>
2025-01-21 18:20:57 +0100

    add a userButton

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
-    <>
-      <p>dashboard page (protected) </p>{" "}
-    </>
+    <div>
+      <p>dashboard page (protected) </p>
+
+      <UserButton />
+    </div>
   );
 };
 
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <div>landing page (Inprotected)</div>;
+  return (
+    <div>
+      landing page (Inprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Registre</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
